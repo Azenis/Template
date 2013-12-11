928138ecd5e3635ff7c7e7f769e1c3f0263dfebd
Azenis <allan-rehhoff@hotmail.com>
2013-12-11 22:07:17 +0100

    testing

--- controller/home.php
+++ controller/home.php
@@ -1,4 +1,2 @@
 <?php $template->message("Congratulations! you're now using a template system", "success"); ?>
-<?php $template->message("Don't forget to add database credentiels in <em>index.php</em>", "attention"); ?>
-
-asd asd
+<?php $template->message("Don't forget to add database credentiels in <em>index.php</em>", "attention"); ?> 
