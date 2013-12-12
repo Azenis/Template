35f761bfeff697436779344d836d9eb7eec12a30
Azenis <allan-rehhoff@hotmail.com>
2013-12-12 20:25:16 +0100

    -

--- controller/home.php
+++ controller/home.php
@@ -3,4 +3,4 @@ if(!strpos($_SERVER["HTTP_HOST"], "rehhoff.me")) {
     $template->message("Congratulations! you're now using a template system", "success");
     $template->message("Don't forget to add database credentiels in <em>index.php</em>", "attention");
 }
-?>  
+?> 
