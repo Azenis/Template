2c1ac294129397994c1355ecd85d87ffa8b6558e
Azenis <allan-rehhoff@hotmail.com>
2013-12-12 19:49:37 +0100

    Version 1.4

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Allan Rehhoff Template
 ===
 Just upload the files to your server and you're ready to go :)
+
+Download: git clone https://github.com/Azenis/Template.git
--- controller/home.php
+++ controller/home.php
@@ -1,2 +1,6 @@
-<?php $template->message("Congratulations! you're now using a template system", "success"); ?>
-<?php $template->message("Don't forget to add database credentiels in <em>index.php</em>", "attention"); ?>  
+<?php
+if(!strpos($_SERVER["HTTP_HOST"], "rehhoff.me")) {
+    $template->message("Congratulations! you're now using a template system", "success");
+    $template->message("Don't forget to add database credentiels in <em>index.php</em>", "attention");
+}
+?>  
